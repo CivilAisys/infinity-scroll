5159100f57a79d21ebf6946e29e40582281624c3
CivilAisys <xx841995@gmail.com>
2022-11-10 01:29:24 +0800

    123

--- script.js
+++ script.js
@@ -11,23 +11,36 @@ const apiKey = '0GTSu1oDSnKJPFm_rsOCofJg28-JHVep3-LjaodnRoc';
 const apiUrl = `https://api.unsplash.com/photos/random/
 ?client_id=${apiKey}&count=${count}`;
 
+
+
+//Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for(const key in attributes){
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 //Create Elements ForLInks & Photos, Add to DOM
-function displayPhotos(){
+function displayPhotos() {
     //Run functino for each object in photosArray
     photosArray.forEach(photo => {
         //Create <a>
         const item = document.createElement('a');
-        item.setAttribute('href', photo.links.html);
-        item.setAttribute('taget', '_blank')
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        })
         //Create <img>
         const img = document.createElement('img');
-        img.setAttribute('src', photo.urls.regular);
-        img.setAttribute('alt', photo.alt_description);
-        img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title:photo.alt_description
+        })
         //Put <img> inside <a>,then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
-        
+
     })
 }
 
@@ -42,6 +55,12 @@ async function getPhotots() {
     }
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () =>{
+    if(window.innerHeight + window.scrollY >= document.body.offsetWidth - 1000){
+        getPhotots();
+    }
+})
 
 //On Load 
 getPhotots();
