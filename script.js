4d8352e56483b711ffb8ed84e7bafbcc71f57f16
CivilAisys <xx841995@gmail.com>
2022-11-10 08:11:26 +0800

    完成

--- script.js
+++ script.js
@@ -2,26 +2,41 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 let photosArray = [];
 
 //Unsplash API
-const count = 10;
+const count = 30;
 const apiKey = '0GTSu1oDSnKJPFm_rsOCofJg28-JHVep3-LjaodnRoc';
 const apiUrl = `https://api.unsplash.com/photos/random/
 ?client_id=${apiKey}&count=${count}`;
 
 
 
+// Check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages){
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+
 //Helper Function to Set Attributes on DOM Elements
 function setAttributes(element, attributes) {
-    for(const key in attributes){
+    for (const key in attributes) {
         element.setAttribute(key, attributes[key]);
     }
 }
 
 //Create Elements ForLInks & Photos, Add to DOM
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('totla images ', totalImages)
     //Run functino for each object in photosArray
     photosArray.forEach(photo => {
         //Create <a>
@@ -35,8 +50,10 @@ function displayPhotos() {
         setAttributes(img, {
             src: photo.urls.regular,
             alt: photo.alt_description,
-            title:photo.alt_description
+            title: photo.alt_description
         })
+        // Even Listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded)
         //Put <img> inside <a>,then put both inside imageContainer Element
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -56,8 +73,11 @@ async function getPhotots() {
 }
 
 // Check to see if scrolling near bottom of page, Load More Photos
-window.addEventListener('scroll', () =>{
-    if(window.innerHeight + window.scrollY >= document.body.offsetWidth - 1000){
+window.addEventListener('scroll', () => {
+    if (window.innerHeight + window.scrollY >= 
+        document.body.offsetWidth - 1000 && ready) {
+        ready = false;
+        loader.hidden = false;
         getPhotots();
     }
 })
